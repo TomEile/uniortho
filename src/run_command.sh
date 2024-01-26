154a2958cd51fb592166e7b9e934c13e957e9b4f
TheOaphidian <tim.van.rillaer@hotmail.com>
2024-01-26 11:28:12 +0100

    Improve installation
    Add prodigal, let user select different pms, link uniortho and R script to decouple script from repo

--- src/run_command.sh
+++ src/run_command.sh
@@ -22,7 +22,6 @@ fi
 check_requirements
 
 
-
 if [[ ! -s "$outf/pan/pangenome.tsv" ]]; then
     prepare_genomes "$data_folder" \
     "$gtdb_url" \
@@ -49,18 +48,12 @@ fi
 
 
 # TODO: Add to path and call from there
-./src/lib/filter_unique_genes.R $outf/pan/pangenome.tsv $outf/ani.af $outf
-fetch_genes $outf
+if [[ ! -s $HOME/.local/bin/uo_filter_unique_genes.R ]]; then
+    echo "uo_filter_unique_genes.R not found in $HOME/.local/bin."
+    echo "Please add the script to your path, for example with"
+    echo "ln -s $PWD/src/lib/uo_filter_unique_genes.R $HOME/.local/bin"
+    exit 1
+fi
 
-#if (( $SECONDS > 3600 )) ; then
-#	    let hours=SECONDS/3600
-#	        let minutes=(SECONDS%3600)/60
-#		    let seconds=(SECONDS%3600)%60
-#		        echo "Completed in $hours hour(s), $minutes minute(s) and $seconds second(s)" 
-#		elif (( $SECONDS > 60 )) ; then
-#			    let minutes=(SECONDS%3600)/60
-#			        let seconds=(SECONDS%3600)%60
-#				    echo "Completed in $minutes minute(s) and $seconds second(s)"
-#			    else
-#				        echo "Completed in $SECONDS seconds"
-#fi
+$HOME/.local/bin/uo_filter_unique_genes.R $outf/pan/pangenome.tsv $outf/ani.af $outf
+fetch_genes $outf
