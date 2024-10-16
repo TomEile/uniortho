3474c18f729c6611ef8151fcec55b42314a6bebc
TheOaphidian <tim.van.rillaer@hotmail.com>
2024-10-16 10:51:53 +0200

    Remove print statement

--- src/test_command.sh
+++ src/test_command.sh
@@ -1,6 +1,5 @@
 blast_check=${args[--blast]}
 if [[ $blast_check == 1 ]]; then
-    echo $blast_check
     uniortho run "s__Sumerlaea chitinivorans" -C 10 -o test_out --blast
 else
     uniortho run "s__Sumerlaea chitinivorans" -C 10 -o test_out
